add9827d62429b5e8d56bbcf5c1bdc8fd74a9b16
Elif Seçkin <74016654+secginelif@users.noreply.github.com>
2022-11-02 20:43:30 +0300

    numbers sum of even and numbers multiply of 4
    find and calculating numbers sum of even and numbers multiply of 4

---
+++ sumofEvenNumbers/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n;
+        int total = 0;
+        //Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //numbers sum of evens and numbers multiple of 4
+        do {
+            System.out.print("Please enter a number: ");
+            n = sc.nextInt();
+            if ((n % 2 == 0) && (n % 4 == 0)) {
+                total += n;
+            }
+
+        }  while(n>0);
+        //print result
+        System.out.print("Total numbers: "+ total);
+
+
+
+    }
+}
