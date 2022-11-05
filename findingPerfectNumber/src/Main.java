c31daf1d5e7e78f5c7c99031274258eaf36b7db2
Elif Seçkin <74016654+secginelif@users.noreply.github.com>
2022-11-05 08:54:25 +0300

    finding perfect number
    finding perfect number by entered number

---
+++ findingPerfectNumber/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //values
+        int number,total=0;
+        //Scanner
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please enter a number: ");
+        number = sc.nextInt();
+
+        //“Perfect numbers” are equal to the sum of their “proper” divisors
+        // (positive integers that divide a number evenly, not counting itself).
+        // example => 6 = 3 + 2 + 1, and 28 = 14 + 7 + 4 + 2 + 1.
+
+        for(int i=1;i<number;i++){
+            if(number %i==0){
+                total += i;
+            }
+        }
+        if(total == number){
+            System.out.print("The number is=> Perfect number");
+        }else{
+            System.out.print("The number is not => Perfect number");
+        }
+    }
+
+}
