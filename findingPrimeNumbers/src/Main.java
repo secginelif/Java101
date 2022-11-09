72696012626e85ecd3c4cf9a1478036ba0917b03
Elif Seçkin <74016654+secginelif@users.noreply.github.com>
2022-11-09 09:53:25 +0300

    finding prime number in 100 numbers

---
+++ findingPrimeNumbers/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // 1 to 100 numbers => finding prime numbers in 100
+        for(int i=2; i<=100; i++){
+            boolean isPrime = true;
+            for(int j=2; j<i; j++){
+                if(i % j == 0){
+                    isPrime = false;
+                }
+            }
+            if(isPrime)
+                System.out.println("--Prime Numbers--");
+                System.out.print(i+ ", ");
+        }
+    }
+}
